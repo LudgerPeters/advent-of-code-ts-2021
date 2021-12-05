045e3bdb2bce10918e170529a9a8a52516d0ba33
Ludger Peters <958414+LudgerPeters@users.noreply.github.com>
2021-12-04 21:26:53 -0500

    Finished Day 1

--- src/day-one/first.ts
+++ src/day-one/first.ts
@@ -5,6 +5,8 @@ export class First {
     let numbers = this.loadFileOfNumbers("src/day-one/fileOfNumbers.txt");
     let count = First.countIncreases(numbers);
     console.log(count);
+    let rollingWindow = First.sumOfRollingWindow(numbers, 3);
+    console.log(First.countIncreases(rollingWindow));
   }
 
   public loadFileOfNumbers(fileName: string): number[] {
@@ -12,6 +14,23 @@ export class First {
     const fileContent = fs.readFileSync(fileName, 'utf8');
     const numbers = fileContent.split('\n').map(Number);
     return numbers;
+  }
+
+  public static sumOfRollingWindow(numbers: number[], windowSize: number): number[] {
+    let sum:number[] = []
+    for (let i = 0; i < numbers.length-(windowSize-1); i++) {
+      let window = numbers.slice(i, i + windowSize);
+      sum.push(First.sumOfArray(window));
+    }
+    return sum;
+  }
+
+  public static sumOfArray(numbers: number[]): number {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
   }
 
   public static countIncreases(input: number[]): number {
--- test/day-one/first.test.ts
+++ test/day-one/first.test.ts
@@ -1,7 +1,12 @@
 import {First} from "../../src/day-one/first";
 
-describe("test count function", () => {
-    it("should return 3 for add(1,2,3,4)", () => {
+describe("test countIncreases", () => {
+    it(" should return 3 for add(1,2,3,4)", () => {
         expect(First.countIncreases([1,2,3,4])).toBe(3)
+    });
+});
+describe("rolling window should create a sum of rolling window based on input", () => {
+    it(" testing simple rolling window", () => {
+        expect(First.sumOfRollingWindow([1,2,3,4],2)).toEqual([3,5,7])
     });
 });
